ff1dba6bb6d9422b61352bee8819b0b89d034969
bromi <bromius@gmail.com>
2023-03-14 02:27:26 +0400

    Add translation answers entity and migration.

---
+++ Data/Entities/TranslationAnswer.cs
@@ -0,0 +1,11 @@
+﻿namespace DsrCourseProjectTranslations.Data.Entities
+{
+    public class TranslationAnswer : BaseEntity
+    {
+        public string Content { get; set; } = null!;
+
+        public int TranslationRequestId { get; set; }
+        public TranslationRequest Request { get; set; } = null!;
+
+    }
+}
