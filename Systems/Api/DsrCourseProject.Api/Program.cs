0e776fa69691e5c3935b1ce80a3620ba07cb65bc
bromi <bromius@gmail.com>
2023-03-27 05:09:50 +0400

    Restructuring to practice

---
+++ Systems/Api/DsrCourseProject.Api/Program.cs
@@ -0,0 +1,35 @@
+using DsrCourseProjectTranslations.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MainDbContext>(
+       options => options.UseSqlServer(
+           builder.Configuration.GetConnectionString("MainContext")));
+
+// Swagger
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
