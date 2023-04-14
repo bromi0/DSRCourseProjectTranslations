0e83ff0b56fdf6118329848db762d69d0ed47eb7
bromi <bromius@gmail.com>
2023-04-14 17:49:11 +0300

    Version annotation

--- Systems/Api/DsrCourseProject.Api/Controllers/LanguagesController.cs
+++ Systems/Api/DsrCourseProject.Api/Controllers/LanguagesController.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using DsrCourseProjectTranslations.Data;
+using DsrCourseProjectTranslations.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using DsrCourseProjectTranslations.Data;
-using DsrCourseProjectTranslations.Data.Entities;
 
 namespace DsrCourseProjectTranslations.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("v{version:apiVersion}/[controller]")]
     [ApiController]
     public class LanguagesController : ControllerBase
     {
@@ -22,24 +17,26 @@ namespace DsrCourseProjectTranslations.Controllers
         }
 
         // GET: api/Languages
+        [ApiVersion("1.0")]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Language>>> GetLanguages()
         {
-          if (_context.Languages == null)
-          {
-              return NotFound();
-          }
+            if (_context.Languages == null)
+            {
+                return NotFound();
+            }
             return await _context.Languages.ToListAsync();
         }
 
         // GET: api/Languages/5
+        [ApiVersion("1.0")]
         [HttpGet("{id}")]
         public async Task<ActionResult<Language>> GetLanguage(int id)
         {
-          if (_context.Languages == null)
-          {
-              return NotFound();
-          }
+            if (_context.Languages == null)
+            {
+                return NotFound();
+            }
             var language = await _context.Languages.FindAsync(id);
 
             if (language == null)
@@ -52,6 +49,7 @@ namespace DsrCourseProjectTranslations.Controllers
 
         // PUT: api/Languages/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [ApiVersion("1.0")]
         [HttpPut("{id}")]
         public async Task<IActionResult> PutLanguage(int id, Language language)
         {
@@ -83,13 +81,14 @@ namespace DsrCourseProjectTranslations.Controllers
 
         // POST: api/Languages
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [ApiVersion("1.0")]
         [HttpPost]
         public async Task<ActionResult<Language>> PostLanguage(Language language)
         {
-          if (_context.Languages == null)
-          {
-              return Problem("Entity set 'MainDbContext.Languages'  is null.");
-          }
+            if (_context.Languages == null)
+            {
+                return Problem("Entity set 'MainDbContext.Languages'  is null.");
+            }
             _context.Languages.Add(language);
             await _context.SaveChangesAsync();
 
@@ -97,6 +96,7 @@ namespace DsrCourseProjectTranslations.Controllers
         }
 
         // DELETE: api/Languages/5
+        [ApiVersion("1.0")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteLanguage(int id)
         {
